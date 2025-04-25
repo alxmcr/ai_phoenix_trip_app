057966a75998ad5213cb907121e2d20b64fba6c4
Alejandro Coca <alxmcr@outlook.com>
2025-04-25 14:15:46 -0400

    chore: create WhereFilterBuilder class

---
+++ helpers/db/builder-where-filter.ts
@@ -0,0 +1,36 @@
+type FilterValue = string | number;
+type Filters<T> = Partial<Record<keyof T, FilterValue>>;
+
+export class WhereFilterBuilder<T> {
+  private filters: string[] = [];
+  private values: FilterValue[] = [];
+
+  where(filters: Filters<T>) {
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value !== undefined && value !== null) {
+        const idx = this.values.length + 1;
+
+        // Check if the field is a string or not
+        const columnType = typeof value;
+
+        // If the column is a string (e.g., name, email),
+        // use ILIKE for partial matching
+        if (columnType === "string") {
+          this.filters.push(`${key} ILIKE $${idx}`);
+          this.values.push(`%${value}%`);
+        } else if (columnType === "number") {
+          // For numbers (e.g., age, id), use exact matching (=)
+          this.filters.push(`${key} = $${idx}`);
+          this.values.push(value as FilterValue);
+        }
+      }
+    });
+
+    // Check if the filters are empty
+    if (this.filters.length === 0) {
+      return "";
+    }
+
+    return `WHERE ${this.filters.join(" AND ")}`;
+  }
+}
