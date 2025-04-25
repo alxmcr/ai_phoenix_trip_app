b462817343e19f23de24333e3d332558eaada42e
Alejandro Coca <alxmcr@outlook.com>
2025-04-25 14:28:33 -0400

    chore: create DBPoolActionables class

--- helpers/db/db-pool-actionables.ts
+++ helpers/db/db-pool-actionables.ts
@@ -9,22 +9,46 @@ export class DBPoolActionables implements DBOperations<ActionableData> {
     this.pool = pool;
   }
 
-  read(id: string): Promise<ActionableData | null> {
-    throw new Error("Method not implemented.");
+  async read(pk_id: string): Promise<ActionableData | null> {
+    const query = `SELECT * FROM actionable WHERE actionable_id = $1`;
+    const result = await this.pool.query(query, [pk_id]);
+    return result.rows[0] || null;
   }
 
-  paginate(page: number, pageSize: number): Promise<ActionableData[]> {
-    throw new Error("Method not implemented.");
+  async paginate(page: number, pageSize: number): Promise<ActionableData[]> {
+    const query = `SELECT * FROM actionable LIMIT $1 OFFSET $2`;
+    const result = await this.pool.query(query, [
+      pageSize,
+      (page - 1) * pageSize,
+    ]);
+    return result.rows;
   }
 
-  update(
-    id: string,
+  async update(
+    pk_id: string,
     item: Partial<ActionableData>
   ): Promise<ActionableData | null> {
-    throw new Error("Method not implemented.");
+    // Build the SET clause
+    const setClause = Object.keys(item)
+      .map((key) => `${key} = $${key}`)
+      .join(", ");
+
+    // Update the actionable
+    const query = `
+      UPDATE actionable
+      SET ${setClause}
+      WHERE actionable_id = $1 RETURNING *
+    `;
+
+    const result = await this.pool.query(query, [
+      pk_id,
+      ...Object.values(item),
+    ]);
+
+    return result.rows[0] || null;
   }
 
-  delete(id: string): Promise<boolean> {
+  delete(pk_id: string): Promise<boolean> {
     throw new Error("Method not implemented.");
   }
 
