59a9e11df15e0740f2efecb2f40e2394f9728cd3
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-25 14:16:24 -0400

    chore: create-filtering-query-builder (#6)
    * refactor: database generics
    
    * chore: create WhereFilterBuilder class

--- generics/db/db-generics.ts
+++ generics/db/db-generics.ts
@@ -1,9 +1,9 @@
 export interface DBOperations<T> {
   read(id: string): Promise<T | null>;
-  filter(filters: Partial<T>): Promise<T[]>;
   paginate(page: number, pageSize: number): Promise<T[]>;
   update(id: string, item: Partial<T>): Promise<T | null>;
   delete(id: string): Promise<boolean>;
   insert(item: Partial<T>): Promise<T>;
   insertMany(items: Partial<T>[]): Promise<T[]>;
+  filter(filters: Partial<T>): Promise<T[]>;
 }
