5b48661500612620e4f89c0c7a6f295c7b767c59
Alejandro Coca <alxmcr@outlook.com>
2025-04-26 19:19:04 -0400

    chore: create actionable action

---
+++ app/actions/create-actionable-action.ts
@@ -0,0 +1,36 @@
+import pool from "@/config/db/db-config";
+import { DBPoolActionables } from "@/helpers/db/db-pool-actionables";
+import { ActionableData } from "@/types/db/actionable";
+import { z } from "zod";
+
+const schema = z.object({
+  review_id: z.string(),
+  priority: z.string(),
+  department: z.string(),
+  category: z.string(),
+  source_aspect: z.string(),
+  title: z.string(),
+  description: z.string(),
+});
+
+export async function createActionable(actionableData: ActionableData) {
+  const dbPoolActionables = new DBPoolActionables(pool);
+
+  const validatedFields = schema.safeParse({
+    review_id: actionableData.review_id,
+    priority: actionableData.priority,
+    department: actionableData.department,
+    category: actionableData.category,
+    source_aspect: actionableData.source_aspect,
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const actionable = await dbPoolActionables.create(actionableData);
+
+  return actionable;
+}
