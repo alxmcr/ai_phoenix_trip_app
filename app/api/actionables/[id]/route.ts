1a7acfc674042fc830e0bd2f61999f5e9a1e9206
Alejandro Coca <alxmcr@outlook.com>
2025-04-26 00:10:43 -0400

    fix: endpoints data values

--- app/api/actionables/[id]/route.ts
+++ app/api/actionables/[id]/route.ts
@@ -4,9 +4,11 @@ import { DBPoolActionables } from "@/helpers/db/db-pool-actionables";
 import { NextRequest, NextResponse } from "next/server";
 
 // GET /api/actionables/:id
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id;
 
   if (!id) {
     return NextResponse.json(
