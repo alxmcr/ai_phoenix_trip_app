54792a3ece29858143a882cd3c7544bd2cbf512d
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-26 16:54:00 -0400

    chore: create endpoint count for sentiments (#27)

---
+++ app/api/sentiments/count/route.ts
@@ -0,0 +1,24 @@
+import pool from "@/config/db/db-config";
+import { HttpResponseCode } from "@/enums/http-response-code";
+import { DBPoolSentiments } from "@/helpers/db/db-pool-sentiments";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const dbPool = new DBPoolSentiments(pool);
+    const count = await dbPool.count();
+
+    // Convert count to number
+    const countNumber = Number(count);
+
+    return NextResponse.json(
+      { count: countNumber },
+      { status: HttpResponseCode.OK }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to get recommendations count" },
+      { status: HttpResponseCode.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
