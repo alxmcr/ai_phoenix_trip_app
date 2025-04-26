2e1ab52aa17b238e4f47a5c14fb08d2c7a814561
Alejandro Coca <alxmcr@outlook.com>
2025-04-26 16:44:35 -0400

    chore: create endpoint count for recommendations

---
+++ app/api/recommendations/count/route.ts
@@ -0,0 +1,24 @@
+import pool from "@/config/db/db-config";
+import { HttpResponseCode } from "@/enums/http-response-code";
+import { DBPoolRecommendations } from "@/helpers/db/db-pool-recommendations";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const dbPool = new DBPoolRecommendations(pool);
+    const count = await dbPool.count();
+
+    // Convert count to number
+    const countNumber = Number(count);
+
+    return NextResponse.json(
+      { count: countNumber },
+      { status: HttpResponseCode.OK }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to get recommendations count" },
+      { status: HttpResponseCode.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
