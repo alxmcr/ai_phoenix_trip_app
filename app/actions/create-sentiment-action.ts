f24b53df9f525f235c14b44f4debd21fb44e146d
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-26 19:10:10 -0400

    chore: create sentiment action (#32)

---
+++ app/actions/create-sentiment-action.ts
@@ -0,0 +1,34 @@
+import pool from "@/config/db/db-config";
+import { DBPoolSentiments } from "@/helpers/db/db-pool-sentiments";
+import { SentimentData } from "@/types/db/sentiment";
+import { z } from "zod";
+
+const schema = z.object({
+  review_id: z.string(),
+  score: z.number(),
+  emotion_tone: z.string(),
+  label: z.string(),
+  summary: z.string(),
+});
+
+export async function createSentiment(sentimentData: SentimentData) {
+  const dbPoolSentiments = new DBPoolSentiments(pool);
+
+  const validatedFields = schema.safeParse({
+    review_id: sentimentData.review_id,
+    score: sentimentData.score,
+    emotion_tone: sentimentData.emotion_tone,
+    label: sentimentData.label,
+    summary: sentimentData.summary,
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const sentiment = await dbPoolSentiments.create(sentimentData);
+
+  return sentiment;
+}
