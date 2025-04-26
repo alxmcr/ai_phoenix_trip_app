4b01cfe4e408e884ae2e505959225511ad78da0f
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-26 14:57:19 -0400

    chore: create aggregate functions and endpoints for reviews (#23)

---
+++ app/api/reviews/count/route.ts
@@ -0,0 +1,18 @@
+import pool from "@/config/db/db-config";
+import { HttpResponseCode } from "@/enums/http-response-code";
+import { DBPoolReviews } from "@/helpers/db/db-pool-reviews";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const dbPool = new DBPoolReviews(pool);
+    const count = await dbPool.count();
+
+    return NextResponse.json({ count }, { status: HttpResponseCode.OK });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to get reviews count" },
+      { status: HttpResponseCode.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
