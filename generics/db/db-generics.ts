62cb625063b75bd57e0bc7c2957f84d93f787e5c
Alejandro Coca <alxmcr@outlook.com>
2025-04-25 22:14:45 -0400

    chore: pagination generic

--- generics/db/db-generics.ts
+++ generics/db/db-generics.ts
@@ -1,8 +1,16 @@
+export interface PaginationParams {
+  page: number;
+  pageSize: number;
+  sortBy: string;
+  sortOrder: "asc" | "desc";
+}
+
 export interface DBOperations<T> {
   findUnique(pk_id: string): Promise<T | null>;
   delete(pk_id: string): Promise<boolean>;
   create(item: Partial<T>): Promise<T>;
   update(pk_id: string, item: Partial<T>): Promise<T | null>;
+  pagination(params: PaginationParams): Promise<T[]>;
 }
 
 export interface DBAggregateFunctions<T> {
