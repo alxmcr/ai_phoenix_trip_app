54d657754a84b3e01b97ba87e693c39bc5ac0ea4
Alejandro Coca <alxmcr@outlook.com>
2025-04-27 02:48:32 -0400

    chore: update list tabels database connection endpoint check

--- app/api/status/db/db-tables-available/route.ts
+++ app/api/status/db/db-tables-available/route.ts
@@ -1,11 +1,34 @@
-import pool from "@/lib/db/db-config";
 import { HttpResponseCode } from "@/enums/http-response-code";
-import { DBPoolHealth } from "@/helpers/db-health/db-pool-health";
+import { PrismaClient, Prisma } from "@/prisma/app/generated/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const dbPoolHealth = new DBPoolHealth(pool);
-  const tables = await dbPoolHealth.checkTablesAvailable();
+const prisma = new PrismaClient();
 
-  return NextResponse.json({ tables }, { status: HttpResponseCode.OK });
+export async function GET() {
+  try {
+    const query = Prisma.sql`
+      SELECT table_name
+      FROM information_schema.tables
+      WHERE table_schema = 'public'
+      AND table_name != '_prisma_migrations'
+    `;
+
+    const result = (await prisma.$queryRaw<{ table_name: string }[]>(
+      query
+    )) as Array<{
+      table_name: string;
+    }>;
+
+    // Extract table names from the result and return them as an array
+    const tables = result.map((row) => row.table_name);
+
+    return NextResponse.json({ tables }, { status: HttpResponseCode.OK });
+  } catch (error) {
+    return NextResponse.json(
+      { error: error },
+      { status: HttpResponseCode.INTERNAL_SERVER_ERROR }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }
