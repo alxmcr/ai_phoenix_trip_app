1e997f0f8173b19cb748afb7cc8318d544379b4d
Alejandro Coca <alxmcr@outlook.com>
2025-04-27 13:51:19 -0400

    refactor: pagination actionables

--- app/api/recommendations/route.ts
+++ app/api/recommendations/route.ts
@@ -4,30 +4,40 @@ import { PaginationResponse } from "@/generics/api/api-generics";
 import { DBPoolRecommendations } from "@/helpers/db/db-pool-recommendations";
 import { RecommendationData } from "@/types/db/recommendation";
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@/prisma/app/generated/prisma";
 
 // GET /api/recommendations?page=1&pageSize=10&sortBy=created_at&sortOrder=desc
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const page = searchParams.get("page") || "1";
   const pageSize = searchParams.get("pageSize") || "10";
+  const sortBy = searchParams.get("sortBy") || "created_at";
+  const sortOrder = searchParams.get("sortOrder") || "desc";
 
-  const dbPoolRecommendations = new DBPoolRecommendations(pool);
+  // Prisma client
+  const prisma = new PrismaClient();
 
-  const recommendations = await dbPoolRecommendations.pagination({
-    page: parseInt(page),
-    pageSize: parseInt(pageSize),
-    sortBy: "created_at",
-    sortOrder: "desc",
+  // Pagination
+  const actionables = await prisma.recommendation.findMany({
+    skip: (parseInt(page) - 1) * parseInt(pageSize),
+    take: parseInt(pageSize),
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
   });
 
-  const buildResponse: PaginationResponse<RecommendationData> = {
-    data: recommendations,
-    total: await dbPoolRecommendations.count(),
+  // Build the pagination response
+  const responsePagination = {
+    total: actionables.length,
     page: parseInt(page),
     pageSize: parseInt(pageSize),
+    totalPages: Math.ceil(actionables.length / parseInt(pageSize)),
+    data: actionables,
   };
 
-  return NextResponse.json(buildResponse, { status: HttpResponseCode.OK });
+  return NextResponse.json(responsePagination, {
+    status: HttpResponseCode.OK,
+  });
 }
 
 // POST /api/recommendations
