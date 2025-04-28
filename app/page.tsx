f88d39b843b872c3852c59371b16110207d85c20
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-28 03:01:18 -0400

    Fix/form review (#57)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { FormReview } from "@/components/forms/form-review";
+import { Toaster } from "react-hot-toast";
+
 export default function Home() {
   return (
     <main>
       <FormReview />
+      <Toaster />
     </main>
   );
 }
