9be55095f135b37eb6fbef3255976c3f824d11a8
Alejandro Coca <alxmcr@outlook.com>
2025-04-29 19:58:11 -0400

    chore: insights section

--- components/sections/review-page/insights-section.tsx
+++ components/sections/review-page/insights-section.tsx
@@ -1,3 +1,28 @@
-export function InsightsSection() {
-  return <div>InsightsSection</div>;
+import InsightsSkeleton from "@/components/skeletons/insights-skeleton";
+import { InsightsTabs } from "@/components/tabs/insight-tabs";
+import { ActionableData } from "@/types/db/actionable";
+import { RecommendationData } from "@/types/db/recommendation";
+import { ReviewData } from "@/types/db/review";
+import { Suspense } from "react";
+
+interface Props {
+  actionables: ActionableData[];
+  recommendations: RecommendationData[];
+}
+
+export function InsightsSection({
+  actionables = [],
+  recommendations = [],
+}: Props) {
+  return (
+    <section className="mb-8 px-4 md:px-0 container">
+      <h2 className="text-2xl font-bold tracking-tight mb-4">
+        Insights & Actions
+      </h2>
+      <InsightsTabs
+        actionables={actionables}
+        recommendations={recommendations}
+      />
+    </section>
+  );
 }
