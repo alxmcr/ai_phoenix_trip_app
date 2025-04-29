2a9add16051a5b90874a9dd0e80965a8959b134e
Alejandro Coca <alxmcr@outlook.com>
2025-04-29 17:07:46 -0400

    chore: create hero section

--- types/prisma/prisma-types.ts
+++ types/prisma/prisma-types.ts
@@ -53,8 +53,21 @@ export interface PrismaRecommendation {
 }
 
 export interface PrismaReviewWithRelations {
-  review: PrismaReview;
-  sentiment: PrismaSentiment;
-  actionables: PrismaActionable[];
-  recommendations: PrismaRecommendation[];
+  review_id: string;
+  rating: number | null;
+  start_date: Date | null;
+  end_date: Date | null;
+  destination: string | null;
+  company_name: string | null;
+  origin: string | null;
+  trip_type: string | null;
+  description: string | null;
+  transport_mode: string | null;
+  email: string | null;
+  age_group: string | null;
+  created_at: Date | null;
+  updated_at: Date | null;
+  sentiment: PrismaSentiment | null;
+  actionables: PrismaActionable[] | null;
+  recommendations: PrismaRecommendation[] | null;
 }
