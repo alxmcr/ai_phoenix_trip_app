5088815793441ba2cf030ba2409ea770152067ee
Alejandro Coca <alxmcr@outlook.com>
2025-04-29 19:23:29 -0400

    chore: insights section

---
+++ components/icons/5x5/impact-icon.tsx
@@ -0,0 +1,18 @@
+import { BarChart, Circle, Target, Zap } from "lucide-react";
+
+type Props = {
+  impact: string;
+};
+
+export function ImpactIcon({ impact = "" }: Props) {
+  switch (impact) {
+    case "high":
+      return <Zap className="h-5 w-5 text-rose-500" />;
+    case "medium":
+      return <Target className="h-5 w-5 text-amber-500" />;
+    case "low":
+      return <BarChart className="h-5 w-5 text-emerald-500" />;
+    default:
+      return <Circle className="h-5 w-5 text-gray-500" />;
+  }
+}
