4091ffba7946d2fde48d96bc74cc2bc152111352
Alejandro Coca <alxmcr@outlook.com>
2025-04-29 02:36:56 -0400

    fix: build issues

--- components/cards/card-form-review.tsx
+++ components/cards/card-form-review.tsx
@@ -1,20 +1,11 @@
-import React from "react";
-import BoxFormReviewSubmitted from "../boxes/box-form-review-submitted";
-import BoxFormReviewSubmitting from "../boxes/box-form-review-submitting";
+import { FormReview } from "../forms/form-review";
 import { Card, CardContent } from "../ui/card";
 
 export default function CardFormReview() {
-  const [review_id, setReviewId] = React.useState<string | null>(null);
-  const [isSubmitting, setIsSubmitting] = React.useState(false);
-  const [isSubmitted, setIsSubmitted] = React.useState(false);
-
   return (
     <Card>
       <CardContent className="pt-6">
-        {isSubmitting && (
-          <BoxFormReviewSubmitting isSubmitting={isSubmitting} />
-        )}
-        {isSubmitted && <BoxFormReviewSubmitted review_id={review_id} />}
+       <FormReview />
       </CardContent>
     </Card>
   );
