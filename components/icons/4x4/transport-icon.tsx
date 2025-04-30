ad82c93f466e9bdec627d9b49d574bafca8c0689
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-30 02:44:01 -0400

    chore: review paginated section (#76)
    * feat: add transport icon and recent review pagination section
    
    - Introduced a new TransportIcon component to display different transport modes.
    - Implemented RecentReviewPaginationSection to manage and display customer reviews with pagination and filtering options.
    
    * chore: reviews paginated section
    
    * chore: reviews paginated section

---
+++ components/icons/4x4/transport-icon.tsx
@@ -0,0 +1,20 @@
+import { Airplay, Bus, Train, Truck } from "lucide-react";
+
+type Props = {
+  mode: string;
+};
+
+export function TransportIcon({ mode = "" }: Props) {
+  switch (mode.toLowerCase()) {
+    case "airplane":
+      return <Airplay className="h-4 w-4 text-blue-500" />;
+    case "train":
+      return <Train className="h-4 w-4 text-green-500" />;
+    case "bus":
+      return <Bus className="h-4 w-4 text-orange-500" />;
+    case "ferry":
+      return <Truck className="h-4 w-4 text-purple-500" />;
+    default:
+      return <Truck className="h-4 w-4 text-gray-500" />;
+  }
+}
