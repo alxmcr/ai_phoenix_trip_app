e78830e32e1aa33fffe315d893839cb5baffab24
Alejandro Coca <alxmcr@outlook.com>
2025-05-01 00:52:21 -0400

    chore: helpers functions dashboards

--- helpers/db/db-pool-reviews.ts
+++ helpers/db/db-pool-reviews.ts
@@ -3,6 +3,8 @@ import {
   DBOperations,
   PaginationParams,
 } from "@/generics/db/db-generics";
+import { PrismaClient } from "@/prisma/app/generated/prisma";
+import { CountByDateMetric } from "@/types/dashboard/types-dashboard";
 import { ReviewData } from "@/types/db/review";
 import { Pool } from "pg";
 
@@ -229,5 +231,44 @@ export class DBPoolReviews implements IDBPoolReviews {
 
     const result = await this.pool.query(query);
     return result.rows;
+  }
+
+  // Prisma: Summary of reviews
+  // Created at 'X': 100 reviews
+  // Created at 'Y': 50 reviews
+  // Created at 'W': 10 reviews
+  async getReviewsByDate() {
+    // Array of created_at dates
+    const prisma = new PrismaClient();
+
+    const createdAtDates = await prisma.review.findMany({
+      select: {
+        created_at: true,
+      },
+    });
+
+    const reviewsByDate = createdAtDates.reduce((acc, review) => {
+      // Extract the date from the created_at field
+      const date = review.created_at?.toISOString().split("T")[0];
+      if (!date) {
+        return acc;
+      }
+
+      if (!acc[date]) {
+        acc[date] = 0;
+      }
+      acc[date]++;
+      return acc;
+    }, {} as Record<string, number>);
+
+    // Convert the reviewsByDate object to an array of objects
+    const reviewsByDateArray: CountByDateMetric[] = Object.entries(
+      reviewsByDate
+    ).map(([date, count]) => ({
+      date,
+      count,
+    }));
+
+    return reviewsByDateArray;
   }
 }
