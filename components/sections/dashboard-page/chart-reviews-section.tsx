8b6760bb14724c47f458580e7ed2d629e823acbb
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-05-01 01:20:24 -0400

    refactor: reviews timeline (#86)
    * chore: helpers functions dashboards
    
    * refactor: update CountReviewsTimeline component to accept data as props and enhance chart rendering
    
    * refactor: enhance CountReviewsTimeline component by removing CartesianGrid and improving axis styling
    
    * feat: add custom tooltip to CountReviewsTimeline for enhanced data display
    
    * refactor: simplify ChartReviewsSection by removing Card wrapper and directly rendering CountReviewsTimeline
    
    * refactor: update color scheme in CountReviewsTimeline to use HSL variables for consistency

--- components/sections/dashboard-page/chart-reviews-section.tsx
+++ components/sections/dashboard-page/chart-reviews-section.tsx
@@ -1,10 +1,19 @@
 import { CountReviewsTimeline } from "@/components/charts/timelines/count-reviews-timeline";
+import { getReviewsByDate } from "@/utils/db/utils-reviews-dashboard";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
+export async function ChartReviewsSection() {
+  const data = await getReviewsByDate();
 
-export function ChartReviewsSection() {
   return (
     <section className="container px-4 md:px-0">
-      <CountReviewsTimeline />
+      <CountReviewsTimeline data={data} />
     </section>
   );
 }
---
+++ types/dashboard/types-dashboard.ts
@@ -0,0 +1,4 @@
+export type CountByDateMetric = {
+  date: string;
+  count: number;
+};
