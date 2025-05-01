cdec49d6e421a10e56d44e4719d11512985a10ed
Alejandro Coca <alxmcr@outlook.com>
2025-04-30 22:04:34 -0400

    chore: charts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,14 @@
+import { CardTimelineReviews } from "@/components/cards/card-timeline-reviews";
 import { ActionablesSection } from "@/components/sections/dashboard-page/actionables-section";
 import { MetricsSection } from "@/components/sections/dashboard-page/metrics-section";
 import { RecentReviewsPaginationSection } from "@/components/sections/dashboard-page/recent-reviews-pagination-section";
 import { TopRecommendationsSection } from "@/components/sections/dashboard-page/top-recommendations-section";
 import { DashboardSkeleton } from "@/components/skeletons/dashboard-skeleton";
+import prisma from "@/lib/prisma";
 import { getMetrics } from "@/utils/db/metrics/get-metrics";
 import { getActionables } from "@/utils/db/utils-actionables";
 import { getTopRecommendations } from "@/utils/db/utils-recomendations";
 import { getRecentReviewsPaginated } from "@/utils/db/utils-reviews-paginated";
-import prisma from "@/lib/prisma";
 import { Suspense } from "react";
 
 export default async function DashboardPage() {
@@ -23,6 +24,8 @@ export default async function DashboardPage() {
     <main className="flex flex-col gap-4 min-h-screen items-center w-full">
       <h1 className="mb-6 text-3xl font-bold tracking-tight">Dashboard</h1>
 
+      <Suspense fallback={<DashboardSkeleton />}></Suspense>
+        <CardTimelineReviews />
       <Suspense fallback={<DashboardSkeleton />}>
         <MetricsSection metrics={metrics} />
       </Suspense>
