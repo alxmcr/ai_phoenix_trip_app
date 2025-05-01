f1d4bc48c37de7c6b4e3c638859c462878098abb
Alejandro Coca <alxmcr@outlook.com>
2025-05-01 02:32:54 -0400

    refactor: count actionables by department

--- components/charts/bar-charts/count-departments-actionables-bar-chart.tsx
+++ components/charts/bar-charts/count-departments-actionables-bar-chart.tsx
@@ -18,37 +18,19 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 import { DepartmentCount } from "@/types/dashboard/types-dashboard";
-const chartData = [
-  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  { browser: "other", visitors: 90, fill: "var(--color-other)" },
-];
 
 const chartConfig = {
-  visitors: {
-    label: "Visitors",
-  },
-  chrome: {
-    label: "Chrome",
+  low: {
+    label: "Low",
     color: "hsl(var(--chart-1))",
   },
-  safari: {
-    label: "Safari",
+  medium: {
+    label: "Medium",
     color: "hsl(var(--chart-2))",
   },
-  firefox: {
-    label: "Firefox",
+  high: {
+    label: "High",
     color: "hsl(var(--chart-3))",
-  },
-  edge: {
-    label: "Edge",
-    color: "hsl(var(--chart-4))",
-  },
-  other: {
-    label: "Other",
-    color: "hsl(var(--chart-5))",
   },
 } satisfies ChartConfig;
 
