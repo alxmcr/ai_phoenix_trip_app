630ea475eda387431bf18b2c98a5346353669f69
Alejandro Coca <alxmcr@outlook.com>
2025-05-01 00:35:50 -0400

    feat: charts review

--- components/charts/pie-charts/count-effort-level-recommendations-pie-chart.tsx
+++ components/charts/pie-charts/count-effort-level-recommendations-pie-chart.tsx
@@ -51,7 +51,7 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-export function EffortLevelRecommendationsPieChart() {
+export function CountEffortLevelRecommendationsPieChart() {
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
